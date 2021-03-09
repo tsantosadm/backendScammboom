ddc9038679cc9a6c74b9d283c8bc33ec14a113fb
DenisNasci <52387592+DenisNasci@users.noreply.github.com>
2021-03-09 16:08:23 -0500

    Sequelize configurado

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'Calasoft60759821',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  }
+}
